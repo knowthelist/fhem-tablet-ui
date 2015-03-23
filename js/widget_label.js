e01beaeaa9a836edcf74a8c172abcf6b96ab15cb
Mario Stephan <mstephan@shared-files.de>
2015-03-23 01:00:20 +0100

    Added meteocons icons

--- js/widget_label.js
+++ js/widget_label.js
@@ -1,6 +1,7 @@
 var widget_label = {
   _label: null,
   elements: null,
+  weathermap: {"heiter":"H","wolkig":"N","Regenschauer":"Q","stark bewoelkt":"Y","Regen":"R","bedeckt":"N","sonnig":"B","Schnee":"U"},
   init: function () {
   	_label=this;
   	_label.elements = $('div[data-type="label"]');
@@ -26,7 +27,16 @@ var widget_label = {
 				fix = ( $.isNumeric(fix) ) ? fix : -1;
 				var val = getPart(value,part);
 				val = ( $.isNumeric(val)  && fix>=0 ) ? Number(val).toFixed(fix) : val;
-				$(this).html( val + "<span style='font-size: 50%;'>"+unit+"</span>" );
+
+				if ($(this).hasClass('weather')){
+					//wheater icons
+					$(this).html('');
+					$(this).attr('data-icon',_label.weathermap[val]);
+					DEBUG && console.log('Label weather icon:'+ _label.weathermap[val]);
+					}
+				else
+					$(this).html( val + "<span style='font-size: 50%;'>"+unit+"</span>" );
+
 				//set colors according limits for numeric values
 				if ($.isNumeric(val)){
 					var limits=$(this).data('limits');
