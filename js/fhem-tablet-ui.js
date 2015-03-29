889a7e3b1289063e8b1719c6c95c73530378db73
Mario Stephan <mstephan@shared-files.de>
2015-03-29 23:20:38 +0200

    Added global date format function

--- js/fhem-tablet-ui.js
+++ js/fhem-tablet-ui.js
@@ -375,3 +375,16 @@ Date.prototype.yyyymmdd = function() {
   var dd  = this.getDate().toString();
   return yyyy+'-'+ (mm[1]?mm:"0"+mm[0])+'-'+(dd[1]?dd:"0"+dd[0]); // padding
  };
+
+Date.prototype.hhmmss = function() {
+  var hh = this.getHours().toString();
+  var mm = this.getMinutes().toString();
+  var ss  = this.getSeconds().toString();
+  return (hh[1]?hh:"0"+hh[0])+':'+ (mm[1]?mm:"0"+mm[0])+':'+(ss[1]?ss:"0"+ss[0]); // padding
+ };
+ 
+ Date.prototype.mmdd = function() {
+  var mm = (this.getMonth()+1).toString(); // getMonth() is zero-based
+  var dd  = this.getDate().toString();
+  return (mm[1]?mm:"0"+mm[0])+'-'+(dd[1]?dd:"0"+dd[0]); // padding
+ };
