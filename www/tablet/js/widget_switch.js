6c1944b75125d3a78e4e6fa7710c76772750f0d2
Thomas Nesges <nesges@gmail.com>
2015-04-27 15:05:33 +0200

    widget_switch als Child von widget_famultibutton

--- www/tablet/js/widget_switch.js
+++ www/tablet/js/widget_switch.js
@@ -1,59 +1,22 @@
-var widget_switch = {
-  _switch: null,
-  elements: null,
-  init: function () {
-  	_switch=this;
-  	_switch.elements = $('div[data-type="switch"]');
- 	_switch.elements.each(function(index) {
+if(typeof widget_famultibutton == 'undefined') {
+    loadplugin('widget_famultibutton');
+}
 
-		var device = $(this).data('device');
-		$(this).data('get', $(this).data('get') || 'STATE');
-		$(this).data('cmd', $(this).data('cmd') || 'set');
-		$(this).data('get-on', $(this).attr('data-get-on') || $(this).attr('data-on') || 'on');
-		$(this).data('get-off', $(this).attr('data-get-off') || $(this).attr('data-off') || 'off');
-		$(this).data('set-on', $(this).attr('data-set-on') || $(this).data('get-on'));
-		$(this).data('set-off', $(this).attr('data-set-off') || $(this).data('get-off'));
-		readings[$(this).data('get')] = true;
-		var elem = $(this).famultibutton({
-			icon: 'fa-lightbulb-o',
-			backgroundIcon: 'fa-circle',
-            offColor: getStyle('.switch.off','color')                       || '#2A2A2A',
-            offBackgroundColor: getStyle('.switch.off','background-color')  || '#505050',
-            onColor: getStyle('.switch.on','color')                         || '#2A2A2A',
-            onBackgroundColor: getStyle('.switch.on','background-color')    || '#aa6900',
-            mode:$(this).hasClass('readonly')?'signal':'toggle',
-			// Called in toggle on state.
-			toggleOn: function( ) {
-				 setFhemStatus($(this).data('cmd')+' '+device+' '+$(this).data('set-on'));
-			},
-			toggleOff: function( ) {
-				 setFhemStatus($(this).data('cmd')+' '+device+' '+$(this).data('set-off'));
-			},
-		});
-	 });
-  },
-  update: function (dev,par) {
-
-    var deviceElements= _switch.elements.filter('div[data-device="'+dev+'"]');
-	deviceElements.each(function(index) {
-        if ( $(this).data('get')==par){
-			var state = getDeviceValue( $(this), 'get' );
-			if (state) {
-				if ( state == $(this).data('get-on') )
-					$(this).data('famultibutton').setOn();
-				else if ( state == $(this).data('get-off') )
-					$(this).data('famultibutton').setOff();
-				else if ( state.match(RegExp('^' + $(this).data('get-on') + '$')) )
-					$(this).data('famultibutton').setOn();
-				else if ( state.match(RegExp('^' + $(this).data('get-off') + '$')) )
-					$(this).data('famultibutton').setOff();
-                else if ( $(this).data('get-off')=='!on' && state != $(this).data('get-on') )
-					$(this).data('famultibutton').setOff();
-                else if ( $(this).data('get-on')=='!off' && state != $(this).data('get-off') )
-                    $(this).data('famultibutton').setOn();
-			}
-		}
-	});
-   }
-			 
-};
+var widget_switch = $.extend({}, widget_famultibutton, {
+    widgetname : 'switch',
+    init: function () {
+        var base = this;
+        this.elements = $('div[data-type="'+this.widgetname+'"]');
+        this.elements.each(function(index) {
+            $(this).data('off-color',               $(this).data('off-color')           || getStyle('.switch.off','color')              || '#2A2A2A');
+            $(this).data('off-background-color',    $(this).data('off-background-color')|| getStyle('.switch.off','background-color')   || '#505050');
+            $(this).data('on-color',                $(this).data('on-color')            || getStyle('.switch.on','color')               || '#2A2A2A');
+            $(this).data('on-background-color',     $(this).data('on-background-color') || getStyle('.switch.on','background-color')    || '#aa6900');
+            $(this).data('background-icon',         $(this).data('background-icon')     || 'fa-circle');
+            $(this).data('icon',                    $(this).data('icon')                || 'fa-lightbulb-o');
+            $(this).data('mode', ($(this).hasClass('readonly')?'signal':'toggle'));
+            base.init_attr($(this));
+            base.init_ui($(this));
+        });
+    },
+});
