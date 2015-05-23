1368f3914d88b7424be2d14e2ddee8bdc3f0ad6b
Johannes <multiwiiparser@gmail.com>
2015-05-23 20:56:28 +0200

    Modified pagetab to work with refresh and unified behavior where all embedded html pages are of the same structure and basic index.html does only contain the menu for initial load

--- www/tablet/js/widget_pagetab.js
+++ www/tablet/js/widget_pagetab.js
@@ -5,77 +5,123 @@ if(typeof widget_famultibutton == 'undefined') {
 var widget_pagetab = $.extend({}, widget_famultibutton, {
     widgetname : 'pagetab',
     lastPage : '',
-    loadPage: function (goUrl){
-        $("div.gridster").fadeOut('fast');
-        localStorage.setItem('reload_'+goUrl,'false');
+    loadPage: function (goUrl, doPush){
+        DEBUG && console.log( 'load page called with : ' + goUrl);
+        if ( doPush ) {
+          history.pushState( history.state, history.title, '#'+goUrl )
+        } else {
+          history.replaceState( history.state, history.title, '#'+goUrl )
+        }
+        // fadeout seems to trouble gridster, so that elements are wrongly sorted on the page
+        //        $('div.gridster').fadeOut('fast');
         $.get(goUrl, function (data_html) {
-            $("div.gridster")
-              .html($(data_html).closest('div.gridster').html())
-              .fadeIn('slow');
+
+          $('div.gridster')
+              .html($(data_html).closest('div.gridster').html());
+//              .fadeIn('slow');
             initPage();
+//            $('div.gridster').fadeIn('slow');
         });
     },
     toggleOn : function(elem) {
         var elem_url=elem.data('url');
         this.lastPage=elem_url;
-        window.location.hash = elem_url;
-        this.loadPage(elem_url);
+        DEBUG && console.log( 'change window location : ' + elem_url);
+//        localStorage.setItem('pagetab_doload', elem_url);  
+        localStorage.setItem('pagetab_doload', 'initializing');  
+//        history.replaceState( history.state, history.title, elem_url )
+//        window.location.hash = elem_url;
+        DEBUG && console.log( 'toggle on with : ' + elem_url);
+        this.loadPage(elem_url, true);
     },
     toggleOff: function(elem) {
           setTimeout(function() {elem.setOn()}, 50);
     },
     init: function () {
         var base = this;
+          
+        DEBUG && console.log( 'init is executed / currently at : ' + window.location);
+//        DEBUG && console.log( 'get localStore (init) to: ' + localStorage.getItem('pagetab'));
         this.elements = $('div[data-type="'+this.widgetname+'"]');
-        this.elements.each(function(index) {
-            $(this).data('off-color',               $(this).data('off-color')           || getStyle('.'+this.widgetname+'.off','color')              || '#606060');
-            $(this).data('off-background-color',    $(this).data('off-background-color')|| getStyle('.'+this.widgetname+'.off','background-color')   || 'transparent');
-            $(this).data('on-color',                $(this).data('on-color')            || getStyle('.'+this.widgetname+'.on','color')               || '#222222');
-            $(this).data('on-background-color',     $(this).data('on-background-color') || getStyle('.'+this.widgetname+'.on','background-color')    || '#606060');
-            $(this).data('background-icon',         $(this).data('background-icon')     || 'fa-circle');
-            $(this).data('mode', 'toggle');
-            base.init_attr($(this));
-            var elem = base.init_ui($(this));
 
-            var elem_url=$(this).data('url');
-            var isCurrent=false;
-            if (filename && elem_url.indexOf(filename)>-1 && !window.location.hash
-                || window.location.hash.indexOf(elem_url,1)>-1
-                || (!filename && elem_url == "index.html" && !window.location.hash ))
-            {
-                isCurrent=true;
-                if ( !window.location.hash ){
-                    window.location.hash = elem_url;
-                    localStorage.setItem('reload_'+elem_url,'false');
+        DEBUG && console.log( 'get localStore pagetab_doload (init) to: ' + localStorage.getItem('pagetab_doload'));
+        var dl = localStorage.getItem('pagetab_doload');
+        if ( ! dl ) {
+          if ( window.location.hash ) {
+            DEBUG && console.log( 'init set doload : ' + window.location.hash);
+            localStorage.setItem('pagetab_doload', window.location.hash.replace('#',''));
+          } else {
+            DEBUG && console.log( 'init set doload : ' + 'home: ' + this.elements.eq(0).data('url'));
+            localStorage.setItem('pagetab_doload', this.elements.eq(0).data('url'));  
+          }
+           var dl = localStorage.getItem('pagetab_doload');
+           DEBUG && console.log( 'init set doload to <initializing> ' );
+           localStorage.setItem('pagetab_doload', 'initializing');  
+           base.loadPage(dl);
+        } else if( ! ( dl == 'initializing' ) ) {
+           DEBUG && console.log( 'redirect init : ');
+           DEBUG && console.log( 'init set doload to <initializing> ' );
+           localStorage.setItem('pagetab_doload', 'initializing');  
+           base.loadPage(dl);
+        } else {
+          DEBUG && console.log( 'normal init : ');
+
+          this.elements.each(function(index) {
+              $(this).data('off-color',               $(this).data('off-color')           || getStyle('.'+this.widgetname+'.off','color')              || '#606060');
+              $(this).data('off-background-color',    $(this).data('off-background-color')|| getStyle('.'+this.widgetname+'.off','background-color')   || 'transparent');
+              $(this).data('on-color',                $(this).data('on-color')            || getStyle('.'+this.widgetname+'.on','color')               || '#222222');
+              $(this).data('on-background-color',     $(this).data('on-background-color') || getStyle('.'+this.widgetname+'.on','background-color')    || '#606060');
+              $(this).data('background-icon',         $(this).data('background-icon')     || 'fa-circle');
+              $(this).data('mode', 'toggle');
+              base.init_attr($(this));
+              var elem = base.init_ui($(this));
+
+              var elem_url=$(this).data('url');
+              var isCurrent=false;
+
+              if ( ! filename ) {
+                if (!window.location.hash ) {
+                  isCurrent = ( index == 0 );
+                } else {
+                  isCurrent = (window.location.hash.indexOf(elem_url,1)>-1);
                 }
-            }
-
-            $(this).attr('title',$(this).data('url'));
-
-            if (isCurrent){
-                elem.setOn();
-                elem.data('on-colors',[elem.data('on-color')]);
-            }
-            else{
-                elem.setOff();
-                elem.data('on-colors',[elem.data('off-color')]);
-            }
-
-            window.onpopstate = function(event) {
-                var hashUrl=window.location.hash.replace('#','');
-                if (base.lastPage != hashUrl){
-                       base.loadPage(hashUrl);
+              } else if (elem_url.indexOf(filename)>-1) {
+                isCurrent = true;
+              } else {
+                if (!window.location.hash ) {
+                  isCurrent = ( index == 0 );
+                } else {
+                  isCurrent = (window.location.hash.indexOf(elem_url,1)>-1);
                 }
-            };
-        });
+              }
 
-        var hashUrl=window.location.hash.replace('#','');
-        if (hashUrl && hashUrl != "index.html" &&
-            localStorage.getItem('reload_'+hashUrl) == 'true')
-            base.loadPage(hashUrl);
-        else
-            localStorage.setItem('reload_'+hashUrl,'true');
+              $(this).attr('title',$(this).data('url'));
 
+              if (isCurrent){
+                  elem.setOn();
+                  elem.data('on-colors',[elem.data('on-color')]);
+              }
+              else{
+                  elem.setOff();
+                  elem.data('on-colors',[elem.data('off-color')]);
+              }
+
+              window.onpopstate = function(event) {
+                  localStorage.setItem('pagetab_doload', 'initializing');  
+                  var hashUrl=window.location.hash.replace('#','');
+                  if ( hashUrl ) {
+                     base.loadPage(hashUrl);
+                  } else {
+                     base.loadPage(base.elements.eq(0).data('url') );
+                  }
+              };
+
+          });
+
+          localStorage.removeItem('pagetab_doload');  
+        
+        }
+        
     },
     update: function (dev,par) {
       var deviceElements= this.elements.filter('div[data-device="'+dev+'"]');
