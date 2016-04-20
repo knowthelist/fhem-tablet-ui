1e2af8a28909e2f0349e4e7ef6f86296d6e707e8
Mario Stephan <mstephan@shared-files.de>
2016-04-20 19:51:18 +0200

    Added update functionality for version 2.2

--- prepare_update.sh
+++ prepare_update.sh
@@ -1,14 +1,14 @@
 #!/bin/bash   
-rm controls_fhemtabletui_eval.txt
+rm controls_fhemtabletui.txt
 find ./www/tablet_eval -type d \( ! -iname ".*" \) -print0 | while IFS= read -r -d '' f; 
   do
    out="DIR $f"
-   echo ${out//.\//} >> controls_fhemtabletui_eval.txt
+   echo ${out//.\//} >> controls_fhemtabletui.txt
 done
 find ./www/tablet_eval -type f \( ! -iname ".*" \) -print0 | while IFS= read -r -d '' f; 
   do
    out="UPD "$(stat -f "%Sm" -t "%Y-%m-%d_%T" $f)" "$(stat -f%z $f)" ${f}"
-   echo ${out//.\//} >> controls_fhemtabletui_eval.txt
+   echo ${out//.\//} >> controls_fhemtabletui.txt
 done
 
 # CHANGED file
