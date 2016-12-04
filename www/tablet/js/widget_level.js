49106f5986ce2145a9a376cfe8457626a6834589
knowthelist <mstephan@shared-files.de>
2016-12-04 20:25:26 +0100

    Update of level and slider widget to provide the same parameter

--- www/tablet/js/widget_level.js
+++ www/tablet/js/widget_level.js
@@ -3,185 +3,37 @@
  * Under MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 
-/* global ftui:true, Modul_widget:true, Powerange: true */
+/* global ftui:true, Modul_slider:true, Powerange: true */
 
 "use strict";
 
 function depends_level() {
-    if (!$.fn.Powerange) {
-        $('head').append('<link rel="stylesheet" href="' + ftui.config.dir + '/../lib/powerange.min.css" type="text/css" />');
-        return ["lib/powerange.min.js"];
+    if (typeof Modul_slider == 'undefined' || !$.fn.Powerange) {
+        return ["slider"];
     }
 }
 
 var Modul_level = function () {
 
-    function init() {
+    function onChange(elem) {}
 
-        me.elements = $('div[data-type="' + me.widgetname + '"]', me.area);
-        me.elements.each(function (index) {
+    function init_attr(elem) {
 
-            var elem = $(this);
-            elem.initData('get', 'STATE');
-            elem.initData('on', 'on');
-            elem.initData('off', 'off');
-            elem.initData('part', -1);
-            elem.initData('min', 0);
-            elem.initData('max', 100);
+        _base.init_attr(elem);
 
-            me.addReading(elem, 'get');
-            // numeric value means fix value, others mean it is a reading
-            if (!$.isNumeric(elem.data('max')))
-                me.addReading(elem, 'max');
-            if (!$.isNumeric(elem.data('min')))
-                me.addReading(elem, 'min');
-
-            var input_elem = $('<input/>', {
-                type: 'text',
-            }).appendTo(elem);
-
-            var pwrng = new Powerange(input_elem[0], {
-                vertical: !elem.hasClass('horizontal'),
-                hideRange: false,
-                'min': elem.data('min') || 0,
-                'max': elem.data('max') || 100,
-                klass: elem.hasClass('horizontal') ? 'level_horizontal' : 'level_vertical',
-            });
-            elem.data('Powerange', pwrng);
-
-            if (elem.hasClass('horizontal')) {
-                if (elem.data('width')) {
-                    elem.css({
-                        'width': $(this).data('width') + 'px',
-                        'max-width': elem.data('width') + 'px',
-                        'height': '0px'
-                    });
-                } else {
-                    if (elem.hasClass('mini'))
-                        elem.css({
-                            'width': '60px',
-                            'max-width': '60px',
-                            'height': '0px'
-                        });
-                    else
-                        elem.css({
-                            'width': '120px',
-                            'max-width': '120px',
-                            'height': '0px'
-                        });
-                }
-                if (elem.data('height')) {
-                    elem.children().find('.range-bar').css({
-                        'height': elem.data('height') + 'px',
-                        'max-height': elem.data('height') + 'px',
-                        'top': '-' + elem.data('height') / 2 + 'px',
-                    });
-                }
-            } else {
-                if (elem.data('height')) {
-                    elem.css({
-                        'height': elem.data('height') + 'px',
-                        'max-height': elem.data('height') + 'px'
-                    });
-                } else {
-                    if (elem.hasClass('mini'))
-                        elem.css({
-                            'height': '60px',
-                            'max-height': '60px'
-                        });
-                    else
-                        elem.css({
-                            'height': '120px',
-                            'max-height': '120px'
-                        });
-                }
-                if (elem.data('width')) {
-                    elem.children().find('.range-bar').css({
-                        'width': elem.data('width') + 'px',
-                        'max-width': elem.data('width') + 'px',
-                        'left': '-' + elem.data('width') / 4 + 'px',
-                    });
-                }
-            }
-
-            elem.addClass(pwrng.options.klass);
-            elem.children().find('.range-handle').css({
-                'visibility': 'hidden',
-                'width': '0px',
-                'height': '0px'
-            });
-        });
-    }
-
-    function update(dev, par) {
-
-        // update from normal state reading
-        me.elements.filterDeviceReading('get', dev, par)
-            .add(me.elements.filterDeviceReading('min', dev, par))
-            .add(me.elements.filterDeviceReading('max', dev, par))
-            .each(function (index) {
-                var elem = $(this);
-                var state = elem.getReading('get').val;
-                if (state) {
-                    var pwrng = elem.data('Powerange');
-                    var input_elem = elem.find('input');
-                    var part = elem.data('part');
-                    var val = ftui.getPart(state, part);
-                    pwrng.options.min = ($.isNumeric(elem.data('min'))) ? elem.data('min') : elem.getReading('min').val;
-                    pwrng.options.max = ($.isNumeric(elem.data('max'))) ? elem.data('max') : elem.getReading('max').val;
-                    if (val == elem.data('off')) val = pwrng.options.min;
-                    if (val == elem.data('on')) val = pwrng.options.max;
-                    if ($.isNumeric(val) && pwrng) {
-
-                        var v = elem.hasClass('negated') ? pwrng.options.max + pwrng.options.min - parseInt(val) : parseInt(val);
-                        if (elem.hasClass('nodelay')) {
-                            pwrng.setStart(parseInt(v));
-                        } else {
-                            // hack for this.slider.offsetHeight=0 issue
-                            setTimeout(function () {
-                                pwrng.setStart(parseInt(v));
-                            }, 250);
-                        }
-
-                        //set colors according matches for values
-                        var limits = elem.data('limits');
-                        if (limits) {
-                            var colors = elem.data('colors');
-
-                            // if data-colors isn't set, try using #505050 instead
-                            if (typeof colors == 'undefined') {
-                                colors = new Array('#505050');
-                            }
-
-                            // fill up colors and icons to states.length
-                            // if an index s isn't set, use the value of s-1
-                            for (var s = 0, len = limits.length; s < len; s++) {
-                                if (typeof colors[s] == 'undefined') {
-                                    colors[s] = colors[s > 0 ? s - 1 : 0];
-                                }
-                            }
-
-                            var idx = ftui.indexOfGeneric(limits, val);
-                            if (idx > -1) {
-                                elem.children().find('.range-quantity').css("background-color", colors[idx]);
-                            }
-                        }
-                        elem.attr('title', val);
-                    }
-                    input_elem.css({
-                        visibility: 'visible'
-                    });
-                }
-            });
+        elem.addClass('readonly');
     }
 
     // public
     // inherit all public members from base class
-    var me = $.extend(new Modul_widget(), {
+    var base = new Modul_slider();
+    var _base = {};
+    _base.init_attr = base.init_attr;
+    var me = $.extend(base, {
         //override or own public members
         widgetname: 'level',
-        init: init,
-        update: update,
+        onChange: onChange,
+        init_attr: init_attr,
     });
 
     return me;
--- www/tablet/js/widget_slider.js
+++ www/tablet/js/widget_slider.js
@@ -16,211 +16,269 @@ function depends_slider() {
 
 var Modul_slider = function () {
 
-    function init() {
+    function elemID(elem) {
+        return me.widgetname + "_" + elem.data("device") + "_" + elem.data('get');
+    }
 
-        me.elements = $('div[data-type="' + me.widgetname + '"]', me.area);
-        me.elements.each(function (index) {
+    function onChange(elem) {
+        var pwrng = elem.data('Powerange');
+        var selMode = elem.data('selection');
+        var v = 0,
+            sliVal = 0;
+        var isunsel = 1;
+        if (pwrng) {
+            isunsel = $(pwrng.slider).hasClass('unselectable');
+            if (isunsel) {
+                elem.data('selection', 1);
+            }
+            sliVal = pwrng.element.value;
+            v = elem.hasClass('negated') ? pwrng.options.max + pwrng.options.min - sliVal : sliVal;
+        }
 
-            var elem = $(this);
+        if (elem.hasClass('value')) {
+            elem.find('#slidervalue').text(v);
+        }
 
-            elem.initData('get', 'STATE');
-            elem.initData('set', '');
-            elem.initData('cmd', 'set');
-            elem.initData('on', 'on');
-            elem.initData('off', 'off');
-            elem.initData('width', null);
-            elem.initData('height', null);
-            elem.initData('value', 0);
-            elem.initData('min', 0);
-            elem.initData('max', 100);
-            elem.initData('step', null);
-            elem.initData('set-value', '$v');
-            elem.initData('get-value', elem.data('part') || -1);
-            elem.initData('color', ftui.getClassColor(elem) || ftui.getStyle('.slider', 'color') || '#aa6900');
-            elem.initData('background-color', ftui.getStyle('.slider', 'background-color') || '#404040');
+        // isunsel == false (0) means drag is over
+        if ((!isunsel) && (selMode)) {
+            if (elem.hasClass('FS20')) {
+                v = ftui.FS20.dimmerValue(v);
+            }
+            elem.data('value', elem.data('set-value').toString().replace('$v', v.toString()));
 
-            me.addReading(elem, 'get');
-
-            var id = elem.data("device") + "_" + elem.data('get');
-
-            var storeval = localStorage.getItem("slider_" + id);
-            storeval = (storeval) ? storeval : '5';
-            var input_elem = $('<input/>', {
-                type: 'text',
-                class: 'slider',
-            }).appendTo(elem);
+            // write visible value (from pwrng) to local storage NOT the fhem exposed value)
+            localStorage.setItem(elemID(elem), sliVal);
+            elem.transmitCommand();
 
             elem.data('selection', 0);
-            var pwrng = new Powerange(input_elem[0], {
-                vertical: !elem.hasClass('horizontal'),
-                min: elem.data('min'),
-                max: elem.data('max'),
-                step: elem.data('step'),
-                tap: elem.hasClass('tap') || false,
-                klass: elem.hasClass('horizontal') ? 'slider_horizontal' : 'slider_vertical',
-                callback: (function () {
-                    var pwrng = elem.data('Powerange');
-                    var selMode = elem.data('selection');
-                    var v = 0,
-                        sliVal = 0;
-                    var isunsel = 1;
-                    if (pwrng) {
-                        isunsel = $(pwrng.slider).hasClass('unselectable');
-                        if (isunsel) {
-                            elem.data('selection', 1);
-                        }
-                        sliVal = pwrng.element.value;
-                        v = elem.hasClass('negated') ? pwrng.options.max + pwrng.options.min - sliVal : sliVal;
-                    }
 
-                    if (elem.hasClass('value')) {
-                        elem.find('#slidervalue').text(v);
-                    }
+        }
+    }
 
-                    // isunsel == false (0) means drag is over
-                    if ((!isunsel) && (selMode)) {
-                        if (elem.hasClass('FS20')) {
-                            v = ftui.FS20.dimmerValue(v);
-                        }
-                        elem.data('value', elem.data('set-value').toString().replace('$v', v.toString()));
+    function init_attr(elem) {
 
-                        // write visible value (from pwrng) to local storage NOT the fhem exposed value)
-                        localStorage.setItem("slider_" + id, sliVal);
-                        elem.transmitCommand();
+        elem.initData('get', 'STATE');
+        elem.initData('set', '');
+        elem.initData('cmd', 'set');
+        elem.initData('on', 'on');
+        elem.initData('off', 'off');
+        elem.initData('width', null);
+        elem.initData('height', null);
+        elem.initData('value', 0);
+        elem.initData('min', 0);
+        elem.initData('max', 100);
+        elem.initData('step', null);
+        elem.initData('set-value', '$v');
+        elem.initData('get-value', elem.data('part') || -1);
+        elem.initData('color', ftui.getClassColor(elem) || ftui.getStyle('.slider', 'color') || '#aa6900');
+        elem.initData('background-color', ftui.getStyle('.slider', 'background-color') || '#404040');
 
-                        elem.data('selection', 0);
+        me.addReading(elem, 'get');
+        // numeric value means fix value, others mean it is a reading
+        if (!$.isNumeric(elem.data('max'))) {
+            me.addReading(elem, 'max');
+        }
+        if (!$.isNumeric(elem.data('min'))) {
+            me.addReading(elem, 'min');
+        }
 
-                    }
-                }).bind(this),
-            });
-            elem.data('Powerange', pwrng);
-            var rangeContainer = elem.find('.range-container');
-            var rangeQuantity = elem.find('.range-quantity');
-            var rangeBar = elem.find('.range-bar');
-            rangeQuantity.css({
-                'background-color': elem.data('color')
-            });
+    }
+
+    function init_ui(elem) {
+
+        var id = elemID(elem);
+
+        var storeval = localStorage.getItem(id);
+        storeval = (storeval) ? storeval : '5';
+
+        var input_elem = $('<input/>', {
+            type: 'text',
+            class: me.widgetname,
+        }).appendTo(elem);
+
+        elem.data('selection', 0);
+
+        var pwrng = new Powerange(input_elem[0], {
+            vertical: !elem.hasClass('horizontal'),
+            min: elem.data('min') || 0,
+            max: elem.data('max') || 100,
+            step: elem.data('step'),
+            tap: elem.hasClass('tap') || false,
+            klass: elem.hasClass('horizontal') ? me.widgetname + '_horizontal' : me.widgetname + '_vertical',
+            callback: function () {
+                me.onChange(elem);
+            }
+        });
+
+        elem.data('Powerange', pwrng);
+
+        var rangeContainer = elem.find('.range-container');
+        var rangeQuantity = elem.find('.range-quantity');
+        var rangeBar = elem.find('.range-bar');
+        rangeQuantity.css({
+            'background-color': elem.data('color')
+        });
+        rangeBar.css({
+            'background-color': elem.data('background-color')
+        });
+
+        if (elem.hasClass('negated')) {
+            var rangeBarColor = rangeBar.css('background-color');
             rangeBar.css({
-                'background-color': elem.data('background-color')
+                'background-color': rangeQuantity.css('background-color')
             });
+            rangeQuantity.css({
+                'background-color': rangeBarColor
+            });
+        }
 
-            if (elem.hasClass('negated')) {
-                var rangeBarColor = rangeBar.css('background-color');
-                rangeBar.css({
-                    'background-color': rangeQuantity.css('background-color')
+        localStorage.setItem(id, storeval);
+
+        if (elem.hasClass('horizontal')) {
+            if (elem.data('width')) {
+                rangeContainer.css({
+                    'width': elem.data('width') + 'px',
+                    'max-width': elem.data('width') + 'px',
+                    'height': '0px'
                 });
-                rangeQuantity.css({
-                    'background-color': rangeBarColor
-                });
-            }
-
-            localStorage.setItem("slider_" + id, storeval);
-
-            if (elem.hasClass('horizontal')) {
-                if (elem.data('width')) {
-                    rangeContainer.css({
-                        'width': elem.data('width') + 'px',
-                        'max-width': elem.data('width') + 'px',
-                        'height': '0px'
-                    });
-                } else {
-                    if (elem.hasClass('mini'))
-                        rangeContainer.css({
-                            'width': '60px',
-                            'max-width': '60px'
-                        });
-                    else
-                        rangeContainer.css({
-                            'width': '120px',
-                            'max-width': '120px'
-                        });
-                }
-                if (elem.data('height')) {
-                    rangeBar.css({
-                        'height': elem.data('height') + 'px',
-                        'max-height': elem.data('height') + 'px',
-                        'top': '-' + elem.data('height') / 2 + 'px',
-                    });
-                }
             } else {
-                if (elem.data('height')) {
+                if (elem.hasClass('mini'))
                     rangeContainer.css({
-                        'height': elem.data('height') + 'px',
-                        'max-height': elem.data('height') + 'px'
+                        'width': '60px',
+                        'max-width': '60px'
                     });
-                } else {
-                    if (elem.hasClass('mini'))
-                        rangeContainer.css({
-                            'height': '60px',
-                            'max-height': '60px'
-                        });
-                    else
-                        rangeContainer.css({
-                            'height': '120px',
-                            'max-height': '120px'
-                        });
-                }
-                if (elem.data('width')) {
-                    rangeBar.css({
-                        'width': elem.data('width') + 'px',
-                        'max-width': elem.data('width') + 'px',
-                        'left': '-' + elem.data('width') / 4 + 'px',
+                else
+                    rangeContainer.css({
+                        'width': '120px',
+                        'max-width': '120px'
                     });
-                }
             }
-
-            if (elem.hasClass('value')) {
-                var lbl = $('<div/>', {
-                    id: 'slidervalue',
-                    class: 'slidertext normal',
-                }).appendTo(rangeContainer);
-            }
-
-            if (elem.hasClass('readonly'))
-                elem.children().find('.range-handle').css({
-                    'visibility': 'hidden'
+            if (elem.data('height')) {
+                rangeBar.css({
+                    'height': elem.data('height') + 'px',
+                    'max-height': elem.data('height') + 'px',
+                    'top': '-' + elem.data('height') / 2 + 'px',
                 });
+            }
+        } else {
+            if (elem.data('height')) {
+                rangeContainer.css({
+                    'height': elem.data('height') + 'px',
+                    'max-height': elem.data('height') + 'px'
+                });
+            } else {
+                if (elem.hasClass('mini'))
+                    rangeContainer.css({
+                        'height': '60px',
+                        'max-height': '60px'
+                    });
+                else
+                    rangeContainer.css({
+                        'height': '120px',
+                        'max-height': '120px'
+                    });
+            }
+            if (elem.data('width')) {
+                rangeBar.css({
+                    'width': elem.data('width') + 'px',
+                    'max-width': elem.data('width') + 'px',
+                    'left': '-' + elem.data('width') / 4 + 'px',
+                });
+            }
+        }
 
-            elem.addClass(pwrng.options.klass);
-            pwrng.setStart(storeval);
+        if (elem.hasClass('value')) {
+            var lbl = $('<div/>', {
+                id: 'slidervalue',
+                class: 'slidertext normal',
+            }).appendTo(rangeContainer);
+        }
 
-            // Refresh slider position after it became visible
-            elem.closest('[data-type="popup"]').on("fadein", function (event) {
-                var storeval = localStorage.getItem("slider_" + id);
-                pwrng.setStart(parseInt(storeval));
+        if (elem.hasClass('readonly')) {
+            elem.children().find('.range-handle').css({
+                'visibility': 'hidden',
+                'width': '0px',
+                'height': '0px'
             });
+        }
+
+        elem.addClass(pwrng.options.klass);
+
+        pwrng.setStart(storeval);
+
+        // Refresh slider position after it became visible
+        elem.closest('[data-type="popup"]').on("fadein", function (event) {
+            var storeval = localStorage.getItem(id);
+            pwrng.setStart(parseInt(storeval));
         });
     }
 
     function update(dev, par) {
         // update from normal state reading
         me.elements.filterDeviceReading('get', dev, par)
+            .add(me.elements.filterDeviceReading('min', dev, par))
+            .add(me.elements.filterDeviceReading('max', dev, par))
             .each(function (index) {
                 var elem = $(this);
                 var state = elem.getReading('get').val;
-                if (state) {
+                if (ftui.isValid(state)) {
                     var pwrng = elem.data('Powerange');
                     var input_elem = elem.find('input');
                     var part = elem.data('get-value') || elem.data('part');
-                    var nstate = ftui.getPart(state, part);
-                    var tstate = nstate;
-                    if (new RegExp('^' + elem.data('on') + '$').test(nstate.toString()))
-                        nstate = pwrng.options.max;
-                    if (new RegExp('^' + elem.data('off') + '$').test(nstate.toString()))
-                        nstate = pwrng.options.min;
-                    if ($.isNumeric(nstate) && input_elem) {
-                        var v = elem.hasClass('negated') ? pwrng.options.max + pwrng.options.min - parseInt(nstate) : parseInt(nstate);
+                    var val = ftui.getPart(state, part);
+                    pwrng.options.min = ($.isNumeric(elem.data('min'))) ? elem.data('min') : elem.getReading('min').val;
+                    pwrng.options.max = ($.isNumeric(elem.data('max'))) ? elem.data('max') : elem.getReading('max').val;
+
+                    if (val > pwrng.options.max) {
+                        val = pwrng.options.max;
+                    }
+                    if (val < pwrng.options.min) {
+                        val = pwrng.options.min;
+                    }
+
+                    var txtValue = val.toString();
+                    if (new RegExp('^' + elem.data('on') + '$').test(txtValue))
+                        val = pwrng.options.max;
+                    if (new RegExp('^' + elem.data('off') + '$').test(txtValue))
+                        val = pwrng.options.min;
+                    if ($.isNumeric(val) && input_elem) {
+                        var v = elem.hasClass('negated') ? pwrng.options.max + pwrng.options.min - parseInt(val) : parseInt(val);
                         pwrng.setStart(parseInt(v));
-                        localStorage.setItem("slider_" + dev + "_" + par, v);
+                        localStorage.setItem(elemID(elem), v);
                         ftui.log(1, 'slider dev:' + dev + ' par:' + par + ' changed to:' + v);
+
+                        //set colors according matches for values
+                        var limits = elem.data('limits');
+                        if (limits) {
+                            var colors = elem.data('colors');
+
+                            // if data-colors isn't set, try using #505050 instead
+                            if (typeof colors == 'undefined') {
+                                colors = new Array('#505050');
+                            }
+
+                            // fill up colors and icons to states.length
+                            // if an index s isn't set, use the value of s-1
+                            for (var s = 0, len = limits.length; s < len; s++) {
+                                if (typeof colors[s] == 'undefined') {
+                                    colors[s] = colors[s > 0 ? s - 1 : 0];
+                                }
+                            }
+
+                            var idx = ftui.indexOfGeneric(limits, val);
+                            if (idx > -1) {
+                                elem.children().find('.range-quantity').css("background-color", colors[idx]);
+                            }
+                        }
+                        elem.attr('title', val);
                     }
                     if (elem.hasClass('value')) {
                         var slidervalue = elem.find('#slidervalue');
                         if (slidervalue) {
                             if (elem.hasClass('textvalue')) {
-                                slidervalue.text(tstate);
+                                slidervalue.text(txtValue);
                             } else {
-                                slidervalue.text(nstate);
+                                slidervalue.text(val);
                             }
                         }
                     }
@@ -236,7 +294,9 @@ var Modul_slider = function () {
     var me = $.extend(new Modul_widget(), {
         //override or own public members
         widgetname: 'slider',
-        init: init,
+        init_ui: init_ui,
+        init_attr: init_attr,
+        onChange: onChange,
         update: update,
     });
 
