b16a832f61656aa94bb5c585313a7af1378869b7
Hermann Ulrichskötter <ulrichsk@gmx.de>
2017-10-05 20:36:10 +0200

    widget_meteogram.js: added properties for width, height, refresh and jsonp-url
    Also updated highcharts-meteogram.js and examples/charts/meteo.html

--- www/tablet/js/widget_meteogram.js
+++ www/tablet/js/widget_meteogram.js
@@ -20,9 +20,21 @@ function depends_meteogram (){
 
 var Modul_meteogram = function() {
 
+    // Is this correct??
+    function startTimer(elem) {
+        var interval = elem.data('refresh');
+        if ($.isNumeric(interval) && interval > 0) {
+            setInterval(function () {
+                refresh(elem);
+            }, Number(interval) * 1000);
+        }
+    }
+
     function init_attr(elem) {
         elem.initData('location'    , 'Tyskland/Berlin/Berlin');
         elem.initData('get'         , 'STATE');
+        elem.initData('refresh'     , '3600');
+        elem.initData('jsonp-url'   , 'http://www.highcharts.com/samples/data/jsonp.php');
 
         this.addReading(elem,'get');
     }
@@ -30,6 +42,21 @@ var Modul_meteogram = function() {
     function refresh(elem) {
         var title = elem.data('title');
         var place = elem.data('location');
+
+	var width     = elem.data('width');
+	var height    = elem.data('height');
+	var jsonp_url = elem.data('jsonp-url');
+
+	if ( width ) {
+	    Meteogram.prototype.getWidth = function () {
+	        return width;
+	    };
+	}
+	if ( height ) {
+	    Meteogram.prototype.getHeight = function () {
+	        return height;
+	    };
+	}
 
         var locationURL = 'http://www.yr.no/place/' + place + '/forecast_hour_by_hour.xml';
 
@@ -45,11 +72,18 @@ var Modul_meteogram = function() {
         // for source code.
 
         $.getJSON(
-            'http://www.highcharts.com/samples/data/jsonp.php?url=' + locationURL + '&callback=?',
+//            'http://www.highcharts.com/samples/data/jsonp.php?url=' + locationURL + '&callback=?',
+            jsonp_url + '?url=' + locationURL + '&callback=?',
             function (xml) {
                 var meteogram = new Meteogram(xml, elem[0]);
             }
         );
+    }
+
+    // Is this correct??
+    function init_ui(elem) {
+        // init interval timer
+        startTimer(elem);
     }
 
     function update(dev, par) {
@@ -66,6 +100,7 @@ var Modul_meteogram = function() {
     return $.extend(new Modul_widget(), {
           widgetname: 'meteogram'
         , init_attr: init_attr
+	, init_ui: init_ui
         , update: update
     });
 };
